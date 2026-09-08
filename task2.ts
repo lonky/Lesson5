1ea383674ee5cb81289d3d5fdff3342544ff79bb
Yan Vashchanka <yan.vashchenko@gmail.com>
2026-09-08 21:30:20 +0300

    feat: task 3

--- task2.ts
+++ task2.ts
@@ -1,1 +1,19 @@
 // Напишите самовызывающиеся функцию подсчета факториала числа: число должно передаваться как параметр функции
+
+const result = (n: number): number => {
+  if (n < 1) {
+    return 1;
+  }
+  return n * result(n - 1);
+};
+
+console.log(result(5));
+
+const result2 = (function fa(n: number): number {
+  if (n < 1) {
+    return 1;
+  }
+  return n * fa(n - 1);
+})(5);
+
+console.log(result2);
--- task3.ts
+++ task3.ts
@@ -1,6 +1,14 @@
 // Напишите функцию, которая при каждом вызове будет выводит увеличенный счетчик на 1
 
 // Пример:
-// const counter = createCounter(10);
-// counter(); // 11
-// counter(); //12
+const counter = createCounter(10);
+console.log(counter()); // 11
+console.log(counter()); // 12
+
+function createCounter(n: number) {
+  let new_n = n;
+  return () => {
+    new_n += 1;
+    return new_n;
+  };
+}
