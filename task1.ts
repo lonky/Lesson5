c67f52a426e7f08595448418d733ff48da03a8ff
Yan Vashchanka <yan.vashchenko@gmail.com>
2026-09-08 20:32:59 +0300

    feat: resolve task 1

--- task1.ts
+++ task1.ts
@@ -1,1 +1,10 @@
 // Написать функцию, которая будет высчитывать сумму чисел от нуля, до параметра, который мы в неё передаем.
+console.log(sum(5));
+
+function sum(n: number): number {
+  let res = 0;
+  for (let count = 1; count <= n; count++) {
+    res += count;
+  }
+  return res;
+}
