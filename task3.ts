d31e9a6ea4e77abd0af0ef4f96f955d224cbe351
Yan Vashchanka <yan.vashchenko@gmail.com>
2026-09-08 22:30:15 +0300

    feat: lesson tasks

--- task3.ts
+++ task3.ts
@@ -2,8 +2,13 @@
 
 // Пример:
 const counter = createCounter(10);
+const counter2 = createCounter(5);
+
 console.log(counter()); // 11
 console.log(counter()); // 12
+
+console.log(counter2()); // 6
+console.log(counter2()); // 7
 
 function createCounter(n: number) {
   let new_n = n;
--- task4.ts
+++ task4.ts
@@ -2,3 +2,31 @@
 // Пароль должен содержать как минимум одну заглавную букву и цифру
 // Пароль только из латинских букв
 // Длина пароля должна быть не менее 10 символов
+
+const validationPassword = (password: string): boolean => {
+  if (password.length < 10) {
+    return false;
+  }
+  let num = 0;
+  let cap = 0;
+  for (const char of password) {
+    if ("0123456789".includes(char)) {
+      num += 1;
+      continue;
+    }
+    if (char.toUpperCase() === char) {
+      cap += 1;
+    }
+  }
+  if (num < 1) {
+    console.log("no num");
+    return false;
+  }
+  if (cap < 1) {
+    console.log("no capital");
+    return false;
+  }
+  return true;
+};
+
+console.log(validationPassword("testte1stTest"));
